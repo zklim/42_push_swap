ab2fcb7b77d7701aa6e9dbf4c0ea7603c447056a
Zhee Khang Lim <zhlim@u90z02s02.42kl.edu.my>
2023-08-04 15:37:54 +0800

    fix: stderr

--- error_handling.c
+++ error_handling.c
@@ -6,21 +6,26 @@
 /*   By: zhlim <zhlim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 17:38:26 by zhlim             #+#    #+#             */
-/*   Updated: 2023/08/03 15:35:39 by zhlim            ###   ########.fr       */
+/*   Updated: 2023/08/04 15:29:51 by zhlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	write_error(char *str)
+{
+	write(2, str, ft_strlen(str));
+}
+
 void	print_error_exit(char *str)
 {
-	ft_printf("%s", str);
+	write_error(str);
 	exit(1);
 }
 
 void	free_error_exit(char *str, t_list *stack)
 {
-	ft_printf("%s", str);
+	write_error(str);
 	ft_lstclear(&stack, destroy_content);
 	exit(1);
 }
